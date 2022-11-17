f07e3b0fa21afd96ae4af9ee59069170cff0d5b8
Vladimir Golubev <vladimirg2107@gmail.com>
2022-11-17 18:36:18 +0300

    User model overwritten

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.User'
