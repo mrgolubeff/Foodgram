6cfcbf663cb0ad7b59afa49cfeb3962db6a3ef0c
Vladimir Golubev <vladimirg2107@gmail.com>
2022-11-21 16:16:02 +0300

    Models described

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,21 +1,80 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 from users.models import User
 
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=200)
-    text = models.TextField()
-    author = models.ForeignKey(
-        User
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название рецепта'
     )
-    image = models.ImageField()
-    cooking_time = models.IntegerField() #Добавить минимальное значение
+    text = models.TextField(
+        verbose_name='Описание рецепта'
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='recipes',
+        on_delete=models.CASCADE,
+        verbose_name='Автор рецепта'
+    )
+    image = models.ImageField(
+        upload_to='recipes/images/',
+        verbose_name='Изображение рецепта'
+    )
+    cooking_time = models.IntegerField(
+        validators=[MinValueValidator(1)],
+        verbose_name='Время приготовления'
+    )
+    tags = models.ManyToManyField(
+        'Tag',
+        related_name='recipes',
+        verbose_name='Теги рецепта'
+    )
+    ingredients = models.ManyToManyField(
+        'Ingredient',
+        through='RecipeIngredient',
+        related_name='recipes',
+        verbose_name='Ингредиенты рецепта'
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
-    pass
+    name = models.CharField(
+        max_length=40,
+        verbose_name='Название тега'
+    )
+    color = models.CharField(
+        max_length=7,
+        verbose_name='Цвет тега (HEX-код)'
+    )
+    slug = models.SlugField(
+        max_length=40,
+        verbose_name='Жетон тега'
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Ingredient(models.Model):
-    pass
+    name = models.CharField(
+        max_length=40,
+        verbose_name='Название ингредиента'
+    )
+    measurement_unit = models.CharField(
+        max_length=15,
+        verbose_name='Единица измерения ингредиента'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.FloatField(validators=[MinValueValidator(0.1)])
